b723dd31704dc7f744613364bd85862a09632856
Zaneta Asare <asarezaneta@gmail.com>
2021-03-12 18:14:30 +0000

    Initialized client-conn

---
+++ Client-Connectivity/src/test/java/com/example/ClientConnectivity/ClientConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClientConnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
